b5405fc837c9605335017e78a621865ccc9023ad
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-19 17:39:34 +0100

    add dynamic cards with javascript

--- js/script.js
+++ js/script.js
@@ -11,15 +11,66 @@ Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSO
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
 */
 
-const baseUrl = 'https://jsonplaceholder.typicode.com/';
-const resource = 'photos';
+
+// Invoco subito la funzione per recuperare le immagini dal server
+getCards();
 
 
-const endpoint = baseUrl + resource;
-const params = {"_limit":6};
-// console.log(endpoint+'?_limit=6');
-console.log(endpoint + params);
+/* Funzione che recupera i dati dal server */
+function getCards() {
+  const baseUrl = 'https://jsonplaceholder.typicode.com/';
+  const resource = 'photos';
 
-axios.get(baseUrl+resource,{params}).then((res)=> {
-  console.log(res.data);
-});
+
+  const endpoint = baseUrl + resource;
+  const params = { "_limit": 6 };
+  // console.log(endpoint+'?_limit=6');
+  console.log(endpoint + params);
+
+  // Creo l'oggetto vuoto che conterrà le cards
+  const objectCards = [{}, {}, {}, {}, {}, {}];
+
+  for (let i = 0; i < 6; i++) {
+    axios.get(baseUrl + resource, { params }).then((res) => {
+      // console.log(res.data);
+
+      // Recupero i singoli elementi e li aggiungo all'oggetto
+      objectCards[i].title = res.data[i].title;
+      objectCards[i].url = res.data[i].url;
+
+
+
+      if (objectCards.length === 6) {
+        // console.log(arrayImage);
+        // console.log(arrayTitle);
+
+        // Una volta ottenuti tutte le immagini e le descrizioni dal
+        // server e popolato objectCards richiamo la funzione drawCards()
+        // che li stampa sulla pagina web
+        drawCards(objectCards);
+      }
+    });
+  }
+}
+
+
+/* Funzione che stampa i dati sulla pagina html */
+function drawCards(objectCards) {
+
+
+  // Recupero l'elemento container dalla pagina html
+  const container = document.getElementById('container');
+  container.innerHTML = '';
+  let cardContainer = document.createElement('figure');
+
+  objectCards.forEach(element => {
+    cardContainer.innerHTML += `
+    <figure class="card-container">
+        <img src="./img/pin.svg" alt="Pin" class="pin-image">
+        <img src="${element.url}" alt="Prova" class="card-image">
+        <figcaption class="title-image">${element.title}</figcaption>
+    </figure>
+    `;
+    container.appendChild(cardContainer);
+  });
+}
